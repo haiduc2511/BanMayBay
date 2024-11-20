e15fbc72f486b471f9ca613b943b96a8a0947b4d
ADMIN <nguyenhuuhaiduc.2003@gmail.com>
2024-11-21 00:02:17 +0700

    add: bottom_menu in MainActivity and Fragments

--- app/src/main/java/com/example/banmaybay2/MainActivity.java
+++ app/src/main/java/com/example/banmaybay2/MainActivity.java
@@ -1,18 +1,62 @@
 package com.example.banmaybay2;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
+
+import com.example.banmaybay2.databinding.ActivityMainBinding;
+import com.google.android.material.navigation.NavigationBarView;
 
 public class MainActivity extends AppCompatActivity {
+    ActivityMainBinding binding;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        initBottomNavigation();
     }
+
+//    private void initBottomNavigation() {
+//        mainFragment = MainFragment.newInstance();
+//        imageUploadListener = mainFragment;
+//        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container_main, mainFragment).commit();
+//        binding.bnMain.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+//            @Override
+//            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+//                int id = menuItem.getItemId();
+//                FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+//                if (R.id.nav_upgrade == id) {
+//                    fragmentTransaction
+//                            .replace(R.id.fragment_container, ManageCategoryFragment.newInstance())
+//                            .commit();
+//                }
+//                if (R.id.nav_store == id) {
+//                    fragmentTransaction
+//                            .replace(R.id.fragment_container, AccountFragment.newInstance())
+//                            .commit();
+//                }
+//                if (R.id.nav_play == id) {
+//                    fragmentTransaction
+//                            .replace(R.id.fragment_container, AccountFragment.newInstance())
+//                            .commit();
+//                }
+//
+//                return true;
+//            }
+//
+//        });
+//    }
+
 }
